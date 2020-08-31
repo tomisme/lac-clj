43c58bab8b193163da74363eddca8de2e570e1b5
Tom Hutchinson <git@tomisme.com>
2020-08-31 20:38:50 +0800

    build category pages and main menu

--- src/lac/build.clj
+++ src/lac/build.clj
@@ -1,5 +1,6 @@
 (ns lac.build
   (:require
+   [portal.api :as p]
    [lac.elements :as el]
    [clojure.edn :as edn]
    [clojure.data.json :as json]
@@ -14,7 +15,15 @@
 
 
 (def all-posts-query
-  "{posts{edges{node{id,modified,modifiedGmt,title,status,slug,uri,date}}}}")
+  "{posts{edges{node{id,modified,modifiedGmt,title,status,slug,uri,date,categories{nodes{name}}}}}}")
+
+
+(def main-menu-query
+  "{menuItems(where:{location:MENU_1}){nodes{path,order,label}}}")
+
+
+(def categories-query
+  "{categories{edges{node{description,name,uri,children{nodes{name}}}}}}")
 
 
 (defn post-content-by-id-query
@@ -22,27 +31,55 @@
   (str "{post(id:\"" id "\"){content}}"))
 
 
+(defn posts-by-category-query
+  [category-name]
+  (str "{posts(where:{categoryName:\"" category-name \""}){edges{node{id,uri}}}}"))
+
+
 (defn query!
   [q]
-  (http/post (:graphql-endpoint config)
-             {:content-type :json
-              :accept :json
-              :body (json/write-str {:query q})}))
+  (-> (http/post (:graphql-endpoint config)
+                 {:content-type :json
+                  :accept :json
+                  :body (json/write-str {:query q})})
+      :body
+      (json/read-str :key-fn keyword)
+      :data))
 
 
-(defn wp-post-content-path
+(def data-path "data")
+
+
+(def main-menu-path
+  (str data-path "/wp-main-menu.edn"))
+
+
+(def categories-path
+  (str data-path "/wp-categories.edn"))
+
+
+(def post-content-path
+  (str data-path "/wp-post-content"))
+
+
+(defn post-content-path-by-id
   [id]
-  (str "data/wp_post_content/" id ".html"))
+  (str post-content-path "/" id ".html"))
 
 
-(defn wp-post-meta-path
+(def post-meta-path
+  (str data-path "/wp-post-meta"))
+
+
+(defn post-meta-path-by-id
   [id]
-  (str "data/wp_post_meta/" id ".edn"))
+  (str post-meta-path "/" id ".edn"))
 
 
-(defn post-response->meta
+(defn post-res->post-meta
   [{:keys [node]}]
   {:post/id (:id node)
+   :categories (-> node :categories :nodes)
    :published-date (:date node)
    :last-modified-date-gmt (:modifiedGmt node)
    :uri (:uri node)
@@ -50,79 +87,181 @@
    :title (:title node)})
 
 
-(defn dl-wp-post-metas!
+(defn dl-post-metas!
   []
-  (doseq [meta (map post-response->meta
+  (fs/mkdirs post-meta-path)
+  (doseq [meta (map post-res->post-meta
                     (-> (query! all-posts-query)
-                        :body
-                        (json/read-str :key-fn keyword)
-                        :data
                         :posts
                         :edges))]
-    (spit (wp-post-meta-path (:post/id meta))
+    (spit (post-meta-path-by-id (:post/id meta))
           meta)))
 
 
-(defn dl-wp-post-content!
+(defn dl-post-content!
   [id]
-  (let [content (-> (post-content-by-id-query id)
-                    query!
-                    :body
-                    (json/read-str :key-fn keyword)
-                    :data
-                    :post
-                    :content)]
-    (spit (wp-post-content-path id)
-          content)))
+  (fs/mkdirs post-content-path)
+  (spit (post-content-path-by-id id)
+        (-> (post-content-by-id-query id)
+            query!
+            :post
+            :content)))
 
 
-(defn get-saved-wp-post-meta-ids
+(defn process-menu-items
+  [coll]
+  (sort-by :order coll))
+
+
+(defn dl-main-menu!
   []
-  (map fs/name (fs/list-dir "data/wp_post_meta")))
+  (spit main-menu-path
+        (-> (query! main-menu-query)
+            :menuItems
+            :nodes
+            process-menu-items)))
 
 
-(defn get-saved-wp-post-metas
+(defn dl-categories!
   []
-  (for [f (fs/list-dir "data/wp_post_meta")]
+  (spit categories-path
+        (->> (query! categories-query)
+            :categories
+            :edges
+            (map :node)
+            (map (fn [category]
+                   (let [children (-> category :children :nodes)]
+                     (if (seq children)
+                       (assoc category :children children)
+                       (dissoc category :children)))))
+            seq)))
+
+
+(defn get-saved-post-meta-ids
+  []
+  (map fs/name (fs/list-dir post-meta-path)))
+
+
+(defn get-saved-post-metas
+  []
+  (for [f (fs/list-dir post-meta-path)]
     (edn/read-string (slurp f))))
 
 
-(defn wp-post-content->hiccup
+(defn get-saved-main-menu
+  []
+  (edn/read-string (slurp main-menu-path)))
+
+
+(defn get-saved-categories
+  []
+  (edn/read-string (slurp categories-path)))
+
+
+(defn get-saved-category-details-by-name
+  [name]
+  (some #(when (= name (:name %))
+           %)
+        (get-saved-categories)))
+
+
+(defn get-saved-posts-by-category-name
+  [name]
+  (filter (fn [{:keys [categories]}]
+            (some #{name} (map :name categories)))
+          (get-saved-post-metas)))
+
+
+(defn build-post-content-hiccup
   [id]
   (map hickory/as-hiccup
-       (-> (wp-post-content-path id)
+       (-> (post-content-path-by-id id)
            slurp
            (hickory/parse-fragment))))
 
 
-(defn build-post-page-hiccup
-  [id]
+(defn build-page-hiccup
+  [{:keys [main]}]
   [:body
-   (el/header-el {:menubar-items [{:name "Blog"}]
+   (el/header-el {:menubar-items (get-saved-main-menu)
                   :header-img-url "https://lightsandclockwork.xyz/static/header-6de1aeeca65f723a4779953f735d7b4d.svg"})
-   (el/main-el
-       (wp-post-content->hiccup id))
+   (el/main-el main)
    (el/footer-el)])
 
 
-#_(wp-post-content-path "cG9zdDo0MA==")
-#_(dl-wp-post-content! "cG9zdDo0MA==")
-#_(into [:div] (wp-post-content->hiccup "cG9zdDo0MA=="))
-#_(build-post-page-hiccup "cG9zdDo0MA==")
-#_(spit "a.html"
-        (-> (build-post-page-hiccup "cG9zdDo0MA==")
+(defn build-post-page-hiccup
+  [id]
+  (build-page-hiccup {:main (build-post-content-hiccup id)}))
+
+
+(defn category-child-el
+  [{:keys [name]}]
+  (let [{:keys [description]} (get-saved-category-details-by-name name)]
+    [:div
+     [:h2 name]
+     [:p description]]))
+
+
+(defn post-preview-el
+  [{:keys [title uri]}]
+  [:li
+   [:a {:href uri}
+    title]])
+
+
+(defn category-page-main-el
+  [{:keys [description name children]}]
+  [:div
+   [:h1 name]
+   [:p description]
+   (if children
+     (into [:div]
+           (map category-child-el children))
+     (into [:ul]
+           (map post-preview-el (get-saved-posts-by-category-name name))))])
+
+
+(defn build-category-page-hiccup
+  [category]
+  (build-page-hiccup {:main (category-page-main-el category)}))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+#_(p/open)
+#_(p/tap)
+
+#_(post-content-path-by-id "cG9zdDo0MA==")
+#_(dl-post-content! "cG9zdDo0MA==")
+#_(tap> (build-post-content-hiccup "cG9zdDo0MA=="))
+#_(tap> (build-post-page-hiccup "cG9zdDo0MA=="))
+#_(tap> (-> (build-post-page-hiccup "cG9zdDo0MA==")
             hiccup/html))
-#_(get-saved-wp-post-meta-ids)
+#_(tap> (get-saved-post-meta-ids))
+#_(tap> (for [category (get-saved-categories)]
+          (build-category-page-hiccup category)))
 
-;; STEP 1: download all wordpress post metadata
-#_(dl-wp-post-metas!)
 
-;; STEP 2: download all wordpress post content
-#_(doseq [id (get-saved-wp-post-meta-ids)]
-    (dl-wp-post-content! id))
+;; download wordpress data
+#_(dl-post-metas!)
+#_(dl-main-menu!)
+#_(dl-categories!)
 
-;; STEP 3: fill up public/ with posts!
-#_(doseq [{:keys [uri post/id]} (get-saved-wp-post-metas)]
+;; download wordpress post content
+#_(doseq [id (get-saved-post-meta-ids)]
+    (dl-post-content! id))
+
+;; build category pages
+#_(doseq [{:keys [uri] :as category} (get-saved-categories)]
+    (let [path (str "public" uri)]
+      (fs/mkdirs path)
+      (spit (str path "index.html")
+            (-> (build-category-page-hiccup category)
+                hiccup/html))))
+
+;; build post pages
+#_(doseq [{:keys [uri post/id]} (get-saved-post-metas)]
     (let [path (str "public" uri)]
       (fs/mkdirs path)
       (spit (str path "index.html")
--- src/lac/elements.clj
+++ src/lac/elements.clj
@@ -22,10 +22,10 @@
             :src header-img-url}]]]
    [:div
     (into [:ul]
-          (for [{:keys [link name]} menubar-items]
+          (for [{:keys [path label]} menubar-items]
             [:li
-             [:a {:href (str "/" link)}
-              name]]))]])
+             [:a {:href path}
+              label]]))]])
 
 
 (defn posted-by-el
@@ -61,17 +61,18 @@
   [:footer
    [:div
     "Thanks for visiting 😃"]
-   [:center
+   [:div
     [:a {:rel "license"
          :href "http://creativecommons.org/licenses/by-nc-sa/4.0/"}
      [:img {:alt "Creative Commons License"
             :style {:border-width 0}
             :src "https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"}]]
-    "This page is licensed under a "
-    [:a {:rel "license"
-         :href "http://creativecommons.org/licenses/by-nc-sa/4.0/"}
-     "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License"]
-    [:p
+    [:div
+     "This page is licensed under a "
+     [:a {:rel "license"
+          :href "http://creativecommons.org/licenses/by-nc-sa/4.0/"}
+      "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License"]]
+    [:div
      "That means it's free to read, share, adapt and reproduce
       for non-commercial purposes, as long as any reproductions
       are also released under the same license, and you give credit."]]])
